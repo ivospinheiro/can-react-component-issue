b7d37b876344cdf7e0cefe44a4d653fc1693b896
Ivo Pinheiro <ivo.pinheiro@nokia.com>
2018-11-06 09:45:40 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import Component from "can-component";
+import canReactComponent from "can-react-component";
+import React from "react";
+import ReactDOM from "react-dom";
+import stache from "can-stache";
+
+const CanJSComponent = Component.extend("InnerComponent", {
+    tag: "inner-component",
+    view: stache("<div class='inner' style='border: solid 1px red;'>Inner text: {{text}}</div>")
+});
+
+const ReactComponent = canReactComponent(CanJSComponent);
+
+class AppComponent extends React.Component {
+    render() {
+        const text = "hello world";
+        return ( <ReactComponent text={text} />);
+    }
+}
+
+ReactDOM.render( <AppComponent /> ,
+    document.getElementById("root")
+);
